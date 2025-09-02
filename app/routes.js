081d68cd2d8625bc5c88d4e4e0f87e9772838e69
stemastr <stevenadi16@gmail.com>
2025-09-02 15:11:37 +0700

    Penguin Dash now support multiple level

--- app/routes.js
+++ app/routes.js
@@ -3,7 +3,7 @@ import { route } from "@react-router/dev/routes";
 export default [
     route( 'games', 'games/root.jsx', [
             route('box-recall/:level', "games/box-recall/App.jsx"),
-            route('penguin-dash', "games/penguin-dash/App.jsx"),
+            route('penguin-dash/:level', "games/penguin-dash/App.jsx"),
             route('connect-things', "games/connect-things/App.jsx"),
         ]
     )
